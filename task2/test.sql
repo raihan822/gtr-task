e465aa9ea63c361f2910d7c714aa38ea42b75b05
raihan822 <raihansarker820@gmail.com>
2025-10-25 23:20:02 +0600

    added db in task2

--- task2/test.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO phones (model_name, brand, display, battery, camera, ram, storage, price_usd, source_url)
-VALUES
-('Samsung Galaxy S23 Ultra', 'Samsung', '6.8" AMOLED', 5000, '200MP + ...', '12GB', '256GB', 1199.00, 'https://www.gsmarena.com/samsung_galaxy_s23_ultra-xxxx.php'),
-('Samsung Galaxy S22 Ultra', 'Samsung', '6.8" AMOLED', 5000, '108MP + ...', '12GB', '256GB', 1099.00, 'https://www.gsmarena.com/samsung_galaxy_s22_ultra-xxxx.php'),
-('Samsung Galaxy A54', 'Samsung', '6.4" AMOLED', 5000, '50MP', '8GB', '128GB', 449.00, 'https://www.gsmarena.com/...');
